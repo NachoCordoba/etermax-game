8de8d66e074b759d2e72b1f2bdcdd47afbeda864
Ignacio A. Cordoba <nachocordoba05@gmail.com>
2022-06-08 00:40:01 -0300

    Refactor test character creation

--- src/classes/Character.test.ts
+++ src/classes/Character.test.ts
@@ -1,11 +1,15 @@
 import Character from "./Character";
 
+const INITIAL_HEALTHPOINT = 10;
+const INITIAL_ALIVE = true;
+const INITIAL_LEVEL = 1;
+
 describe('Iteration one', ()=>{
     it('Character Creation', ()=>{
         const character = new Character();
 
-        expect(character.healthPoint).toEqual(10);
-        expect(character.isAlive).toBeTruthy();
-        expect(character.level).toEqual(1);
+        expect(character.healthPoint).toEqual(INITIAL_HEALTHPOINT);
+        expect(character.isAlive).toEqual(INITIAL_ALIVE);
+        expect(character.level).toEqual(INITIAL_LEVEL);
     });
 })
